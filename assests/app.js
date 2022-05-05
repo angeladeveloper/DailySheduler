c7f7f618fcd3ecec8b98fc040ea8314b02b03b79
Angela W <squittenplease@gmail.com>
2022-05-04 18:06:14 -0700

    importing bs set layout

---
+++ assests/app.js
@@ -0,0 +1,125 @@
+'strict'
+
+// {
+//   name: '5 AM',
+//   time: 0500,
+//   priority: ['low', 'normal', 'high']
+// },
+let hourArray = [
+  {
+    name: '5 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  },
+  {
+    name: '6 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '7 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '8 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '9 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '10 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '11 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '12 AM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '1 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '2 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '3 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '4 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '5 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '6 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '7 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '8 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '9 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+  ,
+  {
+    name: '10 PM',
+    time: 0500,
+    priority: ['low', 'normal', 'high']
+  }
+
+
+]
+
+function renderHours() {
+  hourArray.forEach(hour => {
+    console.log('hi!', hour);
+
+  });
+}
+
